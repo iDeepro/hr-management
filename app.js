152336b10372c555c85e96b926e66cbbafdc4fa9
iDeepro <deeprosengupta@gmail.com>
2020-10-05 22:54:20 +0530

    add name attibute to register.ejs

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+//const session = require('express-session');
+//const passport = require('passport');
+//const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const app = express();
@@ -17,34 +17,40 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(session({
-  secret: "our little secret.",
-  resave: false,
-  saveUninitialized: false
-}));
+// app.use(session({
+//   secret: "our little secret.",
+//   resave: false,
+//   saveUninitialized: false
+// }));
+//
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-mongoose.connect("mongodb://localhost:27017/userDB",
+mongoose.connect("mongodb://localhost:27017/hrDB",
 {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-mongoose.set("useCreateIndex", true);
+//mongoose.set("useCreateIndex", true);
 
-const userScheme = new mongoose.Schema ({
-  email: String,
-  password: String
+const employeeSchema = new mongoose.Schema ({
+  name: String,
+  age: Number,
+  gender: String,
+  empID: String,
+  currPost: String,
+  salary: Number,
+  relatedDept: String,
+  info: String
 });
 
-userScheme.plugin(passportLocalMongoose);
+//userScheme.plugin(passportLocalMongoose);
 
-const User = new mongoose.model("User", userScheme);
+const Employee = new mongoose.model("Employee", employeeSchema);
 
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.use(User.createStrategy());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -67,16 +73,8 @@ app.get("/secrets", (req, res) => {
 });
 
 app.post("/register", (req, res) =>{
-  User.register({username: req.body.username}, req.body.password, (err, user) => {
-    if(err){
-      console.log(err);
-      res.redirect("/register");
-    } else {
-      passport.authenticate("local")(req, res, () => {
-        res.redirect("/secrets");
-      });
-    };
-  });
+  const newEmp = req.body.genSel;
+  console.log(newEmp);
 });
 
 app.post("/login", (req, res)=>{
